a2b339d7163eec0c7316be32fa98860402da16a3
Marcus <taus_i_melek@yahoo.de>
2021-10-17 18:04:35 +0200

    started with snake game

--- cons-sandbox.cpp
+++ cons-sandbox.cpp
@@ -1,51 +1,8 @@
 #include <iostream>
 #include <windows.h>
 
-void Setup()
-{
-}
-
-void Draw();
-
-
-void Input()
-{
-}
-
-void Logic()
-{
-}
-
 int main()
 {
-
-    Draw();
-
+    std::cout << "Hello World";
     return 0;
-}
-
-void Draw()
-{
-
-    for (int x = 0; x < 10; x++)
-    {
-        if (x == 0 || x == 9)
-        {
-            for (int y = 0; y < 32; y++)
-            {
-                std::cout << '-';
-            }
-        }
-        else
-        {
-            std::cout << '|';
-            for (int y = 0; y < 30; y++)
-            {
-                std::cout << ' ';
-            }
-            std::cout << '|';
-        }
-
-        std::cout << "\n";
-    }
 }
